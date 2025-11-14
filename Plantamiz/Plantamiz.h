db85deabdeb447015c5c7ca29d10c2262b4a4e59
Rhodiawithh <rhodia.chanou@edu.ece.fr>
2025-11-14 12:34:57 +0100

    Header

---
+++ Plantamiz/Plantamiz.h
@@ -0,0 +1,33 @@
+#ifndef PLANTAMIZ_H
+#define PLANTAMIZ_H
+
+//librairies
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+
+//Variables 
+
+
+
+
+
+//Structure
+
+
+
+
+
+//Prototypes des sous programmes
+
+
+
+
+
+
+
+
+
+
+#endif //PLANTAMIZ_H
