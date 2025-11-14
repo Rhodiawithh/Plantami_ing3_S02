4d4203cc9c9e9ef0c4019721e1d53c072d6b0092
Rhodiawithh <rhodia.chanou@edu.ece.fr>
2025-11-14 12:29:37 +0100

    Rajot de commentaires pour l'utilisation du header

---
+++ Plantamiz.h
@@ -0,0 +1,21 @@
+
+#ifndef PLANTANIZ_H
+#define PLANTANIZ_H
+
+//Librairies
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+
+//variables globales
+
+
+
+
+//prototypes des sous programmes
+
+
+
+
+#endif //PLANTANIZ_H
---
+++ main.c
@@ -0,0 +1,20 @@
+#include "Plantamiz.h"
+
+
+int main(){
+    char grille[25][45];
+    char lettre[5]={'S','F','O','P','M'}; //les lettres du jeu
+    int carre;
+    
+    
+    
+    for (int i = 0; i < 25; i++) {
+        for (int j = 0; j < 45; j++) {
+            carre = rand() % 5; //ds chaque carré on met notre valeur aleatoire
+            grille[i][j] = lettre[carre]; //ds chaque case on la valeur aleatoire parmi les 5 lettres
+            printf("%c\t",grille[i][j] );
+        }
+        printf("\n"); //saut de ligne pour faire matrice
+    }
+    return 0;
+}
